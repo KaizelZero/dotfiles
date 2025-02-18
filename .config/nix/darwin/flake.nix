65bfb5e795ac66f2ca9f6bbfcbc91fe96c42c4ff
Jimmy Truong <risenalpha@gmail.com>
2025-02-17 21:08:27 -0700

    update nix

--- .config/nix/darwin/flake.nix
+++ .config/nix/darwin/flake.nix
@@ -5,6 +5,7 @@
     nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
     nix-darwin.url = "github:LnL7/nix-darwin/master";
     nix-darwin.inputs.nixpkgs.follows = "nixpkgs";
+    # Includes mas - for looking up Mac App Store IDs
     mac-app-util.url = "github:hraban/mac-app-util";
     nix-homebrew.url = "github:zhaofengli-wip/nix-homebrew";
   };
@@ -23,12 +24,12 @@
           pkgs.ani-cli
           pkgs.nodejs_22
           pkgs.pnpm_10
+          pkgs.odin
         ];
 
         homebrew = {
           enable = true;
           brews = [ 
-            "mas"
             "pyenv"
             "readline"
             "xz"
@@ -71,42 +72,61 @@
           pkgs.nerd-fonts.jetbrains-mono
         ];
 
-        system.defaults = {
-          dock = {
-            autohide = true;
-            largesize = 57;
-            magnification = true;
-            show-recents = false;
-            tilesize = 53;
-            persistent-apps = [
-              "/System/Applications/Messages.app"
-              "/System/Applications/System Settings.app"
-              "/Applications/Brave Browser.app"
-            ];
-            persistent-others = [];
+        system = {
+          stateVersion = 6;
+          configurationRevision = self.rev or self.dirtyRev or null;
+
+          activationScripts.postUserActivation.text = ''
+            # Following line should allow us to avoid a logout/login cycle
+            /System/Library/PrivateFrameworks/SystemAdministration.framework/Resources/activateSettings -u
+          '';
+
+          defaults = {
+            dock = {
+              autohide = true;
+              largesize = 57;
+              magnification = true;
+              show-recents = false;
+              tilesize = 53;
+              persistent-apps = [
+                "/System/Applications/Messages.app"
+                "/System/Applications/System Settings.app"
+                "/Applications/Brave Browser.app"
+              ];
+              persistent-others = [];
+            };
+            loginwindow.GuestEnabled = false;
+            finder = {
+              AppleShowAllExtensions = true;
+              AppleShowAllFiles = true;
+              ShowExternalHardDrivesOnDesktop = false;
+              ShowHardDrivesOnDesktop = false;
+              ShowMountedServersOnDesktop = false;
+              ShowPathbar = true;
+              ShowRemovableMediaOnDesktop = false;
+            };
+            menuExtraClock.ShowSeconds = true;
+            NSGlobalDomain = {
+              AppleInterfaceStyle = "Dark";
+              KeyRepeat = 2;
+              AppleShowAllExtensions = true;
+              AppleShowAllFiles = true;
+              "com.apple.mouse.tapBehavior" = 1;
+            };
           };
-          loginwindow.GuestEnabled = false;
-          finder = {
-            AppleShowAllExtensions = true;
-            AppleShowAllFiles = true;
-            ShowExternalHardDrivesOnDesktop = false;
-            ShowHardDrivesOnDesktop = false;
-            ShowMountedServersOnDesktop = false;
-            ShowPathbar = true;
-            ShowRemovableMediaOnDesktop = false;
-          };
-          menuExtraClock.ShowSeconds = true;
-          NSGlobalDomain = {
-            AppleInterfaceStyle = "Dark";
-            KeyRepeat = 2;
+        };
+
+        system.defaults.CustomUserPreferences = {
+          "com.apple.desktopservices" = {
+            # Avoid creating .DS_Store files on network or USB volumes
+            DSDontWriteNetworkStores = true;
+            DSDontWriteUSBStores = true;
           };
         };
 
         services.nix-daemon.enable = true;
         nix.settings.experimental-features = "nix-command flakes";
         programs.zsh.enable = true;
-        system.configurationRevision = self.rev or self.dirtyRev or null;
-        system.stateVersion = 6;
         nixpkgs.hostPlatform = "aarch64-darwin";
       };
     in
